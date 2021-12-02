5cc107f92a58259dc80e97cbd11b21193a0b09b0
Azurei0 <mhxc90933@gmail.com>
2021-12-02 23:27:24 +0800

    1st working thing i guess

--- App.js
+++ App.js
@@ -1,78 +1,144 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
-import { Dropdown } from 'react-native-material-dropdown-v2';
+import React, { useState, Component } from "react";
+import {
+  View,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Dropdown } from "react-native-material-dropdown-v2";
 
-export default class App extends Component {
-  state = {
-    wordInput: ' ',
-    Venue: ' ',
-    Date: ' ',
-    Time: ' ',
+export const App = () => {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+  const [wordInput, setWordInput] = useState("");
+
+  const eventTypeData = [
+    { value: "Birthday Event" },
+    { value: "Engagement Event" },
+    { value: "Marriage Event" },
+    { value: "Convocation Event" },
+    { value: "Other" },
+  ];
+
+  const venueData = [
+    { value: "Mini Hall" },
+    { value: "Orchid Hall" },
+    { value: "Grand Hall" },
+  ];
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+    console.log(currentDate.toDateString());
+    console.log(currentDate.toTimeString());
+    console.log(wordInput);
   };
 
-  render(){
-  let data = [
-    {value: 'Birthday Event',},
-    {value: 'Engagement Event',},
-    {value: 'Marriage Event',},
-    {value: 'Convocation Event',}];
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
 
-  let data1 = [
-      {value: 'Mini Hall',},
-      {value: 'Orchid Hall',},
-      {value: 'Grand Hall',}];
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
   return (
-    
     <View style={styles.container}>
-      
       <Text style={styles.title}>Event Management System</Text>
-      <Text style={styles.all}>Name</Text> 
-      <TextInput style={styles.input} onChangeText={(wordInput) => this.setState({wordInput})}/>
-      <Text style={styles.all}>Event</Text> 
-      <Dropdown style={styles.inputdropdown} data={data}/>
-      <Text style={styles.all}>Venue</Text> 
-      <Dropdown style={styles.inputdropdown} data={data1}/>
-      <Text style={styles.all}>Date</Text> 
-      <TextInput style={styles.input} onChangeText={(Date) => this.setState({Date})}/>
-      <Text style={styles.all}>Time</Text> 
-      <TextInput style={styles.input} onChangeText={(Time) => this.setState({Time})}/>
-      <StatusBar style="auto" />
+      <View style={styles.rowcontainer}>
+        <Text style={styles.formlabel}>Name</Text>
+        <TextInput
+          style={styles.form}
+          onChangeText={(wordInput) => setWordInput(wordInput)}
+        />
+      </View>
+      <View style={styles.rowcontainer}>
+        <Text style={styles.formlabel}>Event</Text>
+        <Dropdown style={styles.dropdowninput} data={eventTypeData} />
+      </View>
+      <View style={styles.rowcontainer}>
+        <Text style={styles.formlabel}>Venue</Text>
+        <Dropdown style={styles.dropdowninput} data={venueData} />
+      </View>
+
+      <Button
+        style={styles.button}
+        onPress={showDatepicker}
+        title="Choose Date"
+      />
+      <Button
+        style={styles.button}
+        onPress={showTimepicker}
+        title="Choose Time"
+      />
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={false}
+          display="default"
+          onChange={onChange}
+          minimumDate={new Date()}
+        />
+      )}
     </View>
   );
-}
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    margin: 35,
+  },
+  rowcontainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   title: {
-    textAlign: 'center',
+    alignSelf: "center",
+    textAlign: "center",
     fontSize: 25,
-    top : 35,
+    marginTop: 35,
+    marginBottom: 25,
   },
-  all: {
+  formlabel: {
     fontSize: 20,
-    left: 20,
-    top: 60,
   },
-  input: {
-    fontWeight: 'bold',
+  form: {
+    fontWeight: "bold",
     fontSize: 15,
-    backgroundColor: 'lightgrey',
+    backgroundColor: "lightgrey",
     width: 200,
-    left: 150,
-    top: 35,
+    maxHeight: 30,
+    padding: 5,
   },
-  inputdropdown: {
-    fontWeight: 'bold',
+  dropdowninput: {
+    fontWeight: "bold",
     fontSize: 15,
-    backgroundColor: 'lightgrey',
+    backgroundColor: "lightgrey",
     width: 200,
     height: 30,
-    left: 150,
-    top: 35,
-  }
+  },
+  button: {
+    flexDirection: "row",
+    height: 10,
+    justifyContent: "center",
+    alignContent: "space-between",
+    margin: 20,
+  },
 });
